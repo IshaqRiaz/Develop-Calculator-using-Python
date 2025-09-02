8ede2d09c15bc1cff46166518862c730600a78fc
IshaqRiaz <99590439+IshaqRiaz@users.noreply.github.com>
2025-09-02 17:20:32 +0500

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,45 @@
+print("Welcome to the Calculator project")
+while True:
+    print("Enter your q to quit at any time")
+    #getting the first input from the user
+    first_number = input("Please enter your first number: ")
+    #termination of calculation due to "q" selection
+    if first_number == "q":
+        print("Calculation has been Terminated!")
+        break
+    #conversion of input to float
+    first_number = float(first_number)
+    #getting the second input from the user
+    second_number = input("Please enter your second number: ")
+    #termination of calculation due to "q" selection
+    if second_number == "q":
+        print("Calculation has been Terminated!")
+        break
+    #conversion of input to float
+    second_number = float(second_number)
+    #Main program execution
+    operation = input("Which operation do you want to perform i.e +,-,/,*: ")
+    # + operation
+    if operation == "+":
+        sum  = first_number + second_number
+        print("The sum (+) of Both numbers are: ", sum)
+    # - operation
+    elif operation == "-":
+        difference  = first_number - second_number
+        print("The Difference (-) of Both numbers are: ", difference)
+    # * operation
+    elif operation == "*":
+        multiply  = first_number * second_number
+        print("The Multiply (*) of Both numbers are: ", multiply)
+    # / operation
+    elif operation == "/":
+        #controlling of devident by zero
+        if second_number == 0:
+            print("Error: 0 can't be used as a devident")
+        else:
+            result = first_number / second_number
+            print("The devident (/) of Both numbers are: ", result)
+else:
+    print("Invalid choice")
+    
+print("Thank you for your time")
